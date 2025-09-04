f159b6ce7257414d973172d0c165993a06cff0e5
GuillaumeSUBRA <guillaumesubra1@gmail.com>
2025-09-04 13:44:47 +0200

    update home

--- flutter/crypto/lib/home/home.dart
+++ flutter/crypto/lib/home/home.dart
@@ -14,7 +14,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('test'),
+        title: const Text('Cryptographie', style: TextStyle(fontWeight: FontWeight.bold)),
       ),
       body:const Text("Hello World"),
     );
