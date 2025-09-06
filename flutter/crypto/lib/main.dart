4e8d219f88a8f64862538962516a4384e2a4e8fd
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2025-09-06 18:10:19 +0200

    add darkmode

--- flutter/crypto/lib/main.dart
+++ flutter/crypto/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:crypto/home/home.dart';
+import 'package:crypto/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,12 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      theme: ThemeData(useMaterial3: true),
       home: const Home(),
     );
   }
 }
-
