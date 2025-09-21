7a4f7b0eb3c32e1aa2d44cb074ea39a85a617a1e
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2025-09-21 07:47:14 +0200

    fix name

--- flutter/crypto/lib/shared/decoration.dart
+++ flutter/crypto/lib/shared/decoration.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 // Decoration pour les textfield
-textFieldDecoration(Widget child, double s) {
+textFieldDecoration(Widget child, double size) {
   return Container(
       margin: const EdgeInsets.symmetric(vertical: 10),
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-      width: s * 0.8,
+      width: size * 0.8,
       decoration: BoxDecoration(
         color: Colors.black,
         borderRadius: BorderRadius.circular(30),
