daa23d6741edaf6dd2c4fb918a19673e2a27d95a
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2025-09-22 09:07:47 +0200

    add utils

---
+++ flutter/crypto/lib/shared/utils.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+/// Supprime les accents des lettres
+String removeAccents(String input) {
+  final accentsMap = {
+    'à': 'a',
+    'â': 'a',
+    'ä': 'a',
+    'é': 'e',
+    'è': 'e',
+    'ê': 'e',
+    'ë': 'e',
+    'î': 'i',
+    'ï': 'i',
+    'ô': 'o',
+    'ö': 'o',
+    'ù': 'u',
+    'û': 'u',
+    'ü': 'u',
+    'ç': 'c',
+    'À': 'A',
+    'Â': 'A',
+    'Ä': 'A',
+    'É': 'E',
+    'È': 'E',
+    'Ê': 'E',
+    'Ë': 'E',
+    'Î': 'I',
+    'Ï': 'I',
+    'Ô': 'O',
+    'Ö': 'O',
+    'Ù': 'U',
+    'Û': 'U',
+    'Ü': 'U',
+    'Ç': 'C',
+  };
+
+  return input.characters.map((char) => accentsMap[char] ?? char).join();
+}
+
+/// vérifier si le code ascii correspond à une lettre
+bool isLetter(int ascii) {
+  return (ascii >= 65 && ascii <= 90) || (ascii >= 97 && ascii <= 122);
+}
+
+/// verifier si le code ascii correspond à une lettre majuscule
+bool isUpper(int char) {
+  return char >= 65 && char <= 90;
+}
